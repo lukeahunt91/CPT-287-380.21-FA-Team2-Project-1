53472f2c45d36a8b7cd367a8eb549442a5946daa
lukeahunt91 <91393041+lukeahunt91@users.noreply.github.com>
2021-10-04 21:43:12 -0500

    Added Movie_Iterator and Movie_Iterable interfaces
    Created and added custom iterator and iterable classes due to type mismatch with built in Linked_List iterator.

---
+++ Project_1/src/project_1/Movie_Iterable.java
@@ -0,0 +1,10 @@
+package project_1;
+
+public interface Movie_Iterable {
+
+	/**
+	 * Generates an iterator at the beginning of the data structure 
+	 * @return
+	 */
+	Movie_Iterator iterator();
+}

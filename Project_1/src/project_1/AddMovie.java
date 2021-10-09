01e82fbb04acc7dc48d2dd4f5614767cd56d9eff
lukeahunt91 <91393041+lukeahunt91@users.noreply.github.com>
2021-10-08 19:47:22 -0500

    Cleanup
    Add close for scanner. Added comments. Added whitespace for readability.

--- Project_1/src/project_1/AddMovie.java
+++ Project_1/src/project_1/AddMovie.java
@@ -6,33 +6,41 @@ import java.util.Scanner;
 
 public class AddMovie {
 	
+	/**
+	 * Uses scanner to prompt user for fields to create a new movie object.
+	 * @return: movie object if no errors occur; {null} otherwise
+	 */
 	public static Movie addMovie(){
 
 		try {
-			Scanner mScanner = new Scanner(System.in);
+			Scanner mScanner = new Scanner(System.in); // Create scanner to read user inputs
 
-			System.out.println("Please enter the movie title: ");
+			System.out.println("Please enter the movie title: "); // Prompt for movie title
+			String movieTitle = mScanner.nextLine(); // Read title with scanner
+			
+			System.out.println("Please enter the movie release date in mm/dd/yyyy format: "); // Prompt for release date
+			String movieRelease = mScanner.nextLine(); // Read release date with scanner
+			Date movieRDate = new SimpleDateFormat("MM/dd/yyyy").parse(movieRelease); // Format input
+			
+			System.out.println("Please enter the movie description: "); // Prompt for description
+			String movieDescription = mScanner.nextLine(); // Read description with scanner
+			
+			System.out.println("Please enter the movie receive date in mm/dd/yyyy format: "); // Prompt for receive date
+			String movieReceive = mScanner.nextLine(); // Read receive date with scanner
+			Date movieRcDate = new SimpleDateFormat("MM/dd/yyyy").parse(movieReceive); // Format input
+			
+			String movieStatus = "RECEIVED"; // Set movie status to RECEIVED
 
-			String movieTitle = mScanner.nextLine();
-			System.out.println("Please enter the movie release date in mm/dd/yyyy format: ");
-			String movieRelease = mScanner.nextLine();
-			Date movieRDate = new SimpleDateFormat("MM/dd/yyyy").parse(movieRelease);
-			System.out.println("Please enter the movie description: ");
-			String movieDescription = mScanner.nextLine();
-			System.out.println("Please enter the movie recieve date in mm/dd/yyyy format: ");
-			String movieReceive = mScanner.nextLine();
-			Date movieRcDate = new SimpleDateFormat("MM/dd/yyyy").parse(movieReceive);
-			String movieStatus = "RECEIVED";
-
-			Movie movie = new Movie(movieTitle, movieRDate, movieDescription, movieRcDate, movieStatus); 
-			return movie; 
-		} catch (Exception e) {
-			System.out.println("Invalid input");
+			Movie movie = new Movie(movieTitle, movieRDate, movieDescription, movieRcDate, movieStatus); // Create new movie object with input data
+			mScanner.close(); // Close scanner
+			return movie; // Return movie object
 		}
+		
+		catch (Exception e) { // If error occurs
+			System.out.println("Invalid input"); // Provide error message
+		}
+		
+		return null; // Return null with error
 
-		return null;
-	}
-	
-	
-
-}
+	} // End of method
+} // End of Class
